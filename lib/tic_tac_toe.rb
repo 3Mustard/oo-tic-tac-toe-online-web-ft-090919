b9c1bc02335d64ed93a8e8a977e9f18a13dab355
Matthew C <m23.cullen@gmail.com>
2019-09-20 06:32:45 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -54,16 +54,23 @@ class TicTacToe
   end 
   
   def won?
-    WIN_COMBINATIONS.detect do |combo|
-      if (@board[combo[0]] == "X") && (@board[combo[1]] == "X") && (@board[combo[2] == "X"]) 
-        return combo 
-      elsif (@board[combo[0]] == "O") && (@board[combo[1]] == "O") && (@board[combo[2] == "O"])
-        return combo
-      else 
-        return false 
+    WIN_COMBINATIONS.each do |win_combo|
+      index_0 = win_combo[0]
+      index_1 = win_combo[1]
+      index_2 = win_combo[2]
+
+      position_1 = @board[index_0]
+      position_2 = @board[index_1]
+      position_3 = @board[index_2]
+
+      if position_1 == "X" && position_2 == "X" && position_3 == "X"
+        return win_combo
+      elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+        return win_combo
       end
     end 
-  end 
+    return false
+  end
 end 
 
 describe "#won?" do
