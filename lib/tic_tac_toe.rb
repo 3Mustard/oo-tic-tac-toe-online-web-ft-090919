5258ccb980f5ff48bee51483914e92ba575625b2
Matthew C <m23.cullen@gmail.com>
2019-09-20 07:01:15 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -82,7 +82,11 @@ class TicTacToe
   end 
   
   def winner
-    
+    WIN_COMBINATIONS.detect do |combo|
+      @board[combo[0]] == @board[combo[1]] &&
+      @board[combo[1]] == @board[combo[2]] &&
+      (@board[combo[0]] == "X" || @board[combo[0]] == "O")
+    end 
   end 
 end 
 
