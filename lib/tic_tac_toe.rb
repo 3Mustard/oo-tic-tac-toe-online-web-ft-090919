888a55136dd187229cf30785021fd91ce8e2ab25
Matthew C <m23.cullen@gmail.com>
2019-09-20 06:30:57 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -57,7 +57,7 @@ class TicTacToe
     WIN_COMBINATIONS.detect do |combo|
       if (@board[combo[0]] == "X") && (@board[combo[1]] == "X") && (@board[combo[2] == "X"]) 
         return combo 
-      elsif ((@board[combo[0]] == "O") && (@board[combo[1]] == "O") && (@board[combo[2] == "O"]))
+      elsif (@board[combo[0]] == "O") && (@board[combo[1]] == "O") && (@board[combo[2] == "O"])
         return combo
       else 
         return false 
