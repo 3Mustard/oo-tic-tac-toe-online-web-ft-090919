1b722d20dbd379469a358b425c7ece1bd307a724
Matthew C <m23.cullen@gmail.com>
2019-09-20 07:06:23 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -81,14 +81,17 @@ class TicTacToe
     full? || won? ? true : false 
   end 
   
-  def winner
-    combo = won? 
-    if @board[combo[0]] == "X"
-      return "X"
-    elsif @board[combo[0]] == "O"
-      return "O"
-    end 
-  end 
+  def winner 
+    WIN_COMBINATIONS.detect do |combo| 
+      if @board[combo[0]] == "X" && @board[combo[1]] == "X" && @board[combo[2]] == "X" 
+        return "X"
+      elsif @board[combo[0]] == "O" && @board[combo[1]] == "O" && @board[combo[2]] == "O"
+        return "O"
+      else 
+        nil
+      end
+    end
+  end
 end 
 
 
